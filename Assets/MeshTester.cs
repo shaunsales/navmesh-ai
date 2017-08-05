0873de77a31dc6e1022a6b9aa682bf6fc6fb3a2d
Shaun Lelacheur Sales <shaun@cmune.com>
2017-08-05 12:38:38 +0800

    Initial Unity test project.

---
+++ Assets/MeshTester.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshTester : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+}
